1dd150b0bfb3b55d55571e56c9cc51f2bfc16c8c
Franz Holzinger <franz.holzinger@festwein.de>
2024-08-01 19:09:36 +0200

    TYPO3 13

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,7 +9,7 @@ $EM_CONF[$_EXTKEY] = [
   'title' => 'TYPO3 Skin Icons',
   'description' => 'This library offers the t3skin icons.',
   'category' => 'misc',
-  'version' => '1.0.0',
+  'version' => '1.0.1',
   'state' => 'stable',
   'uploadfolder' => 0,
   'clearcacheonload' => 0,
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = [
   [
     'depends' =>
     [
-      'typo3' => '10.4.0-12.5.99',
+      'typo3' => '10.4.0-13.5.99',
     ],
     'suggests' =>
     [
